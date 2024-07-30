955e4c9646a7bdf92a3b7ff9c368da270d8ee91c
nidal-intelo <nidal@intelo.ai>
2024-07-30 22:06:37 +0530

    - added new packages:  - mongodb to connect to db  - nodemon to automatically restart the server on every update save

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
+const url = process.env.MONGODB_URI;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+MongoClient.connect(url)
+  .then(client => {
+    console.log('Connected to Database');
 
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
 
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  })
+  .catch(error => console.error(error));
